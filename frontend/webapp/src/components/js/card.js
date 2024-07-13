b28701b9f9eae45abeac6b0d60d0f11fb0e803bf
void173 <handeprathmesh1@gmail.com>
2024-07-13 21:26:12 +0530

    deleted my card css file and image and updated

--- frontend/webapp/src/components/js/card.js
+++ frontend/webapp/src/components/js/card.js
@@ -1,16 +1,45 @@
-import '../css/card.css';
-function Card(props) {
-    return(
-        <div className="cardbody"> 
-            <div className="cardtitle"> <p>{props.title}</p></div>
-            <div className="cardcontent">
-                <div className="carddesc"><p>{props.desc}</p></div>
-                <div className="cardimg">
-                    <img src={props.img} alt="card-image"></img>
-                </div>
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import JobImage from '../../assets/Mangetsajob.png';
+
+function CustomCard() {
+  const cardTitleStyle = {
+    fontFamily: 'Poppins, sans-serif',
+    fontWeight: 700,
+    fontSize: '24px',
+    lineHeight: '36px',
+    marginBottom: '10px',
+    width: '100%', // Adjust width as needed
+  };
+
+  const cardTextStyle = {
+    fontFamily: 'Poppins, sans-serif',
+    fontWeight: 400,
+    fontSize: '16px',
+    lineHeight: '24px',
+  };
+
+  return (
+    <div className="row justify-content-center">
+      {/* First Card */}
+      <div className="col-md-6 col-lg-4 mb-4">
+        <Card style={{ width: '100%', background: '#F4F6FA' }}>
+          <Card.Body className="d-flex">
+            <div style={{ paddingRight: '20px', flex: '1' }}>
+              <Card.Title style={cardTitleStyle}>Back Office operations</Card.Title>
+              <Card.Text style={cardTextStyle}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.
+              </Card.Text>
             </div>
-        </div>
-    );
+            <div style={{ flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <Image src={JobImage} style={{ width: '100%', height: 'auto', maxWidth: '210px' }} />
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
-export default Card;
+export default CustomCard;
--- frontend/webapp/src/pages/js/About.js
+++ frontend/webapp/src/pages/js/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Card from '../../components/js/card';
-import img from "../../assets/img1.png";
+
 const abstyle = {
     textAlign:'center'
 };
@@ -8,7 +7,6 @@ export default function About() {
   return (
     <>
     <h1 style={abstyle}>This is about page</h1>
-    <Card title="Personal Loan Recovery" img={img} desc="We provide personal loan recovery services.fhvosb fhds fdav fdsb brw fdwvd dvfdv"/>
     </>
   )
 }
