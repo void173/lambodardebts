f1dd67511af0f2a9a49f7659a2d59dbd39530ea6
Mandar <mandargurjar51@gmail.com>
2024-07-13 22:58:10 +0530

    Made footer responsive

--- frontend/webapp/src/components/js/footer.js
+++ frontend/webapp/src/components/js/footer.js
@@ -1,52 +1,57 @@
-import React from 'react'
-import call from '../../assets/Call.png'
-import gmail from '../../assets/Gmail.png'
-import loc from '../../assets/Location.png'
-import '../css/footer.css'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import call from '../../assets/Call.png';
+import gmail from '../../assets/Gmail.png';
+import loc from '../../assets/Location.png';
+import '../css/footer.css';
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
     <>
-    <div className='footer-main'>
+      <div className='footer-main'>
         <div className='footer-content'>
-            <div className='footer-content-contact'>
-                <h4>Contact Us</h4>
-                <div>
-                    <img src={call} alt=' '></img><span>  919860485555 , 0240-2980505
-                    Ganesh</span>
-                </div>
-                <div>
-                    <img src={gmail} alt=' '></img><span>  Ganesh.mansurkar@moryaassociates.com</span>
-                </div>
-                <br/>
+          <div className='footer-content-contact'>
+            <h4>Contact Us</h4>
+            <br/>
+            <div>
+              <img src={call} alt=' ' /><span> 919860485555 , 0240-2980505 Ganesh</span>
             </div>
-            <div className='footer-content-quicklinks'>
-                <h4>Quick Links</h4>
-                <div>
-                    <ul>
-                        <li><Link to='/home'>Home</Link></li>
-                        <li><Link to='/about'>About Us</Link></li>
-                        <li><Link to='/services'>Services</Link></li>
-                        <li><Link to='carrer'>Carriers</Link></li>
-                    </ul>
-                </div>
+            <br/>
+            <div>
+              <img src={gmail} alt=' ' /><span> Ganesh.mansurkar@moryaassociates.com</span>
             </div>
-            <div className='footer-content-ourLocation'>
-                <h4>Our Locations</h4>
-                <div>
-                    <img src={loc} alt=''></img><span><a href='w.com'>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur </a></span>
-                </div>
-                <div>
-                    <img src={loc} alt=''></img><span><a href='w.com'>  Lorem ipsum dolor sit amet consectetur adipisicing elit.t ing elit. Lorem ipsum dolor sit amet consectetur</a></span>
-                </div>
+            <br />
+          </div>
+          <div className='footer-content-quicklinks'>
+            <h4>Quick Links</h4>
+            <br/>
+            <div>
+              <ul>
+                <li><Link to='/home'>Home</Link></li>
+                <li><Link to='/about'>About Us</Link></li>
+                <li><Link to='/services'>Services</Link></li>
+                <li><Link to='/career'>Careers</Link></li>
+              </ul>
             </div>
+          </div>
+          <div className='footer-content-ourLocation'>
+            <h4>Our Locations</h4>
+            <br/>
+            <div>
+              <img src={loc} alt='' /><span><a href='w.com'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur </a></span>
+            </div>
+            <br/>
+            <div>
+              <img src={loc} alt='' /><span><a href='w.com'> Lorem ipsum dolor sit amet consectetur adipisicing elit.t ing elit. Lorem ipsum dolor sit amet consectetur</a></span>
+            </div>
+          </div>
         </div>
-        <br/>
-        <br/>
+        <br />
+        <br />
         <div className='footer-copyright'>
-            <p style={{margin:"0px"}}>@2024 Lambodar Debt Solution. All Right reserved </p>
+          <p style={{ margin: "0px" }}>@2024 Lambodar Debt Solution. All Right reserved </p>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
