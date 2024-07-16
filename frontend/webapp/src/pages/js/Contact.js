cebf8853f8ec73bf13b31ae2f3e8b66f088ead9e
Ayush kesharwani <ayush.kesharwani@gvilab.com>
2024-07-16 20:08:36 +0530

    Sujal Commit

--- frontend/webapp/src/pages/js/Contact.js
+++ frontend/webapp/src/pages/js/Contact.js
@@ -4,9 +4,37 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button'; 
 import contactImage from '../../assets/contactUs.png'; 
+import '../css/contact.css';
+import callImage from '../../assets/Call_Contact.png'
+import emailImage from '../../assets/Email.png'
 
 function Contact() {
   return (
+    <section>
+    <div className="container">
+    <div className="left-container">
+      <div className="text-container"> {/* New container for the paragraph */}
+        <h2>Get in touch</h2>
+        <p>Visit our agency or simply send us an email anytime you want. If you have any questions, please feel free to contact us.</p>
+      </div>
+    </div>
+    <div className="right-container">
+      <div className="contact-info">
+        <div className="contact-item">
+          <img src={callImage} alt="Call" />
+          <p>9322944343 / 7058417001
+          </p>
+        </div>
+        <div className="contact-item">
+          <img src={emailImage} alt="Email" />
+          <p>lambodardebtsolution@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
     <div
       className="background-image"
       style={{
@@ -21,7 +49,7 @@ function Contact() {
     >
       <div style={{ width: '100%', padding: '20px' }}>
         <h1 style={{ textAlign: 'left', marginBottom: '30px', paddingLeft: '20px' }}>Contact Us</h1>
-        <Form style={{ maxWidth: '600px', backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: '20px', borderRadius: '10px' }}>
+        <Form style={{ maxWidth: '600px', padding: '20px', borderRadius: '10px' }}>
           <Form.Group className="mb-3">
             <Form.Control type="text" placeholder="Name" />
           </Form.Group>
@@ -40,6 +68,7 @@ function Contact() {
         </Form>
       </div>
     </div>
+    </section>
   );
 }
 
