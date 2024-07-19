e0d3f519dc8c730f9f2cad21ef22387d2c48f840
Mandar <mandargurjar51@gmail.com>
2024-07-19 17:22:56 +0530

    Added aos

--- frontend/webapp/src/pages/js/Services.js
+++ frontend/webapp/src/pages/js/Services.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React , {useEffect}from 'react';
 import img1 from '../../assets/online-business.png'
 import Cards from '../../components/js/Card' 
 import { Container } from 'react-bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // Import your Card component here
 import '../css/services.css'
 const abstyle = {
@@ -9,6 +11,13 @@ const abstyle = {
 };
 
 const Services = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Global duration for all animations
+      once: false, // Whether animation should happen only once - while scrolling down
+      mirror: false, // Whether elements should animate out while scrolling past them
+    });
+  }, []);
   return (
     <Container>
     <section>
@@ -16,12 +25,12 @@ const Services = () => {
       
       <div className='first-section'>
         <div className='first-section-left'>
-          <h4>Affordable services provided by us.</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.</p>
-          <span id='btn'><button >Explore</button></span>
+          <h4 data-aos="fade-right" data-aos-once="true">Affordable services provided by us.</h4>
+          <p data-aos="fade-left" data-aos-once="true">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.</p>
+          <span id='btn' data-aos="fade-up" data-aos-once="true"><button >Explore</button></span>
         </div>
         <div className='first-section-right'>
-          <img src={img1} alt="online business" className='img-fluid'></img>
+          <img src={img1} alt="online business" className='img-fluid' data-aos="fade-left"></img>
         </div>
         
       </div>
