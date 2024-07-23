84c965573e489eb10d5743a72d9f7a95d497a6ee
Mandar <mandargurjar51@gmail.com>
2024-07-23 23:53:39 +0530

    Added new banks in carousel

--- frontend/webapp/src/components/js/Carousel.js
+++ frontend/webapp/src/components/js/Carousel.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 // import ExampleCarouselImage from '../../components/js/ExampleCarouselImage'; // Ensure this component exists and handles rendering of images
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
-import kotak from '../../assets/kotak.png'; // Import kotak.png
-import sbi from '../../assets/sbi.png'; // Import sbi.png
-import hdfc from '../../assets/hdfc.png'; // Import the third image
+import heroFinance from '../../assets/heroFincorp.png'
+import bajajFinance  from '../../assets/bajajFinance.png';
+import LandT from '../../assets/LandT.png';
 import loksuvidha from '../../assets/loksuvidha.png'
 
 function IndividualIntervalsExample() {
@@ -13,28 +13,28 @@ function IndividualIntervalsExample() {
       <Carousel.Item interval={1500}>
         <img
           className="d-block w-100"
-          src={sbi}
+          src={loksuvidha}
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100"
-          src={kotak}
+          src={bajajFinance}
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={500}>
         <img
           className="d-block w-100"
-          src={hdfc}
+          src={LandT}
           alt="Third slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={300}>
         <img
           className="d-block w-100"
-          src={loksuvidha}
+          src={heroFinance}
           alt="Third slide"
         />
       </Carousel.Item>
