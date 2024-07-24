b1373694d378d409f37c3295ed617eaff1e9ccb9
Mandar <mandargurjar51@gmail.com>
2024-07-24 07:57:43 +0530

    Trying to remove the buttons of slider

--- frontend/webapp/src/components/js/ReactCardSlider.js
+++ frontend/webapp/src/components/js/ReactCardSlider.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import '../css/reactCard.css'
 const ReactCardSlider = ({ slides }) => {
   return (
     <div className="react-card-slider">
       {slides.map((slide, index) => (
-        <div className="slide" key={index} onClick={slide.clickEvent}>
-          <img src={slide.image} alt={slide.title} style={slide.style} draggable="false"/>
+        <div className="slide" key={index}>
+          <img src={slide.image} alt={slide.title} style={slide.style} draggable="false" />
           <h3>{slide.title}</h3>
           <p>{slide.description}</p>
         </div>
